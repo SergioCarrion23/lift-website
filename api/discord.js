e8f50455e63c06178f417a4bb799cf2a6ee521b6
Sergio Carrion <108313215+SergioCarrion23@users.noreply.github.com>
2026-03-12 00:26:51 -0500

    Add files via upload

--- api/discord.js
+++ api/discord.js
@@ -1,4 +1,4 @@
-const DISCORD_WEBHOOK = 'https://discord.com/api/webhooks/1481476406037840058/s2BjeP9nNOhMlCYfvNTdJYLK1hM8PoMYPSO0DBezHBRaa9wapfhGoj3VekGnJbHwVyXT';
+const DISCORD_WEBHOOK = 'https://discord.com/api/webhooks/1481523450630897739/x9v9yCanaJ6v4dCZaqK1sWEl4PmeZAgC3Ik-8WO6wm_daqZ_panaOEGCPWec4bv-E5D6';
 
 export default async function handler(req, res) {
   console.log('Method:', req.method);
